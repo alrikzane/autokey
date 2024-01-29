88e6b9838091a0b65624b82b795ce2ed7f86f264
Aleksey <ckill071@gmail.com>
2024-01-29 13:38:56 +0800

    get list of winows with sertain name and click in each of them

--- wothelper.py
+++ wothelper.py
@@ -6,4 +6,18 @@ import time
 
 mouse = Controller()
 
+#get list of all windows, which include "езым" in their name
+win = gw.getAllWindows()
+wot_objects = []
+for x in win:
+    if 'езым' in x.title: wot_objects.append(x)
 
+
+for x in wot_objects:
+    mouse.position = (500, 400)
+    x.activate()
+    time.sleep(0.05)
+    mouse.press(Button.left)
+    mouse.move(0,50)
+    mouse.release(Button.left)
+    
